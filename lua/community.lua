16649fbf0803340aae257e7bb581e724148261ef
ak2ume <ak2ume@users.noreply.github.com>
2024-05-12 13:18:43 +0900

    add modes.nvim configuration

--- lua/community.lua
+++ lua/community.lua
@@ -13,4 +13,5 @@ return {
   { import = "astrocommunity.register.nvim-neoclip-lua" },
   { import = "astrocommunity.git.fugit2-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.color.modes-nvim" },
 }
