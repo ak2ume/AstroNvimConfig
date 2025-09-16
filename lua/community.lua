42efb55a7fa16e70e235afa0ce347b2e33b4fbe7
ak2ume <ak2ume@users.noreply.github.com>
2025-09-16 18:21:10 +0900

    uninstall fugit2

--- lua/community.lua
+++ lua/community.lua
@@ -11,7 +11,6 @@ return {
   -- import/override with your plugins folder
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
-  { import = "astrocommunity.git.fugit2-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.color.modes-nvim" },
   { import = "astrocommunity.search.nvim-hlslens" },
